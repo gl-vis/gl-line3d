681398a5477f64cfd92ff7494215d5b6854e834b
archmoj <mojtaba@plot.ly>
2018-10-01 15:29:01 -0400

    adjust scales to handle negative cases, fix for reversed bounds

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -9,10 +9,17 @@ varying vec3    worldPosition;
 varying float   pixelArcLength;
 varying vec4    fragColor;
 
+bool outOfRange(float a, float b, float p) {
+  if (p > max(a, b)) return true;
+  if (p < min(a, b)) return true;
+  return false;
+}
+
 void main() {
-  if(any(lessThan(worldPosition, clipBounds[0])) || any(greaterThan(worldPosition, clipBounds[1]))) {
-    discard;
-  }
+  if (outOfRange(clipBounds[0].x, clipBounds[1].x, worldPosition.x)) discard;
+  if (outOfRange(clipBounds[0].y, clipBounds[1].y, worldPosition.y)) discard;
+  if (outOfRange(clipBounds[0].z, clipBounds[1].z, worldPosition.z)) discard;
+
   float dashWeight = texture2D(dashTexture, vec2(dashScale * pixelArcLength, 0)).r;
   if(dashWeight < 0.5) {
     discard;
--- shaders/pick.glsl
+++ shaders/pick.glsl
@@ -9,9 +9,16 @@ varying vec3 worldPosition;
 varying float pixelArcLength;
 varying vec4 fragColor;
 
+bool outOfRange(float a, float b, float p) {
+  if (p > max(a, b)) return true;
+  if (p < min(a, b)) return true;
+  return false;
+}
+
 void main() {
-  if(any(lessThan(worldPosition, clipBounds[0])) || any(greaterThan(worldPosition, clipBounds[1]))) {
-    discard;
-  }
+  if (outOfRange(clipBounds[0].x, clipBounds[1].x, worldPosition.x)) discard;
+  if (outOfRange(clipBounds[0].y, clipBounds[1].y, worldPosition.y)) discard;
+  if (outOfRange(clipBounds[0].z, clipBounds[1].z, worldPosition.z)) discard;
+
   gl_FragColor = vec4(pickId/255.0, packFloat(pixelArcLength).xyz);
 }
